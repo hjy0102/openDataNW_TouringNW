51953d7943a747c65ec3ba67c741966ab2d40e09
Hyojin Yi <hyojinyi@live.ca>
2017-02-25 11:36:54 -0800

    rename

---
+++ TouringNW_ios/Pods/Target Support Files/Pods-TouringNWTests/Pods-TouringNWTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TouringNWTests : NSObject
+@end
+@implementation PodsDummy_Pods_TouringNWTests
+@end
---
+++ TouringNW_ios/Pods/Target Support Files/Pods-TouringNWUITests/Pods-TouringNWUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TouringNWUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TouringNWUITests
+@end
